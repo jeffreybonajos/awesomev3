c5a30138a26a917e29d7a6fe6811b36facaeab81
Jeffrey Bonajos <jbonajos@fabletics.com>
2019-09-30 23:23:37 +0800

    no message

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-function Home() {
-    return <div>Welcome to Next.js!</div>
-  }
-  
-  export default Home
-  
+import React from 'react'
+import Link from 'next/link'
+
+export default () => (
+  <ul>
+    <li>
+      <Link href='/b' as='/a'>
+        <a>a</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/a' as='/b'>
+        <a>b</a>
+      </Link>
+    </li>
+  </ul>
+)
