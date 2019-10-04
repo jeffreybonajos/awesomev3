8ec23ab16ba095429a63b1cebc260d2bf0672e29
jeffreybonajos <jeffrey.bonajos@gmail.com>
2019-10-04 18:57:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const express = require("express");
+const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+  const server = express();
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+  server.get("/index", (req, res) => {
+    return app.render(req, res, "/index");
+  });
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
