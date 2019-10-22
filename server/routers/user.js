611350b6f9e771709a165353e223487404a15dc5
holdensumili <sumiliholden@gmail.com>
2019-10-22 22:07:20 +0800

    push

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,42 +1,41 @@
-const express = require('express')
-const router = new express.Router()
-const userModel = require('../database/models/user')
-const authMiddleware = require('../middleware/auth')
-const jwt = require('jsonwebtoken')
 
-router.post('/api/login', async (req, res) => {
-  const { username, password } = req.body
   try {
-    const user = await userModel.findByCredentials(username, password)
-    if(!user){
-      res.status(500).send()
     }
-    const token = jwt.sign({ user_id: user.user_id.toString() }, 'awesomeversionthree')
     res.json({
       success: true,
-      message: 'Authentication successful!',
       token: token
-    })
-  } catch(e){
-    res.status(400).send(e)
   }
-})
 
-router.get('/api/homepage', authMiddleware, async (req, res) => {
-  res.send(token)
-}) 
-
-router.post('/api/logout', authMiddleware, async (req, res) => {
+router.get("/api/profile", async (req, res) => {
   try {
     req.token = []
     await req.user.save()
     res.send()
   } catch(e) {
     res.status(500).send()
+    const user = await userModel.getUserProfile(4);
+    if (!user) {
+      res.status(500).send();
+    }
+    res.json(user);
+    console.log(user);
+  } catch {
+    console.log(e);
+    res.status(500).send();
   }
 })
 
 
+});
 
+router.post("/api/logout", authMiddleware, async (req, res) => {
+  try {
+    req.token = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
 module.exports = router;
